b3c5b14bda7f4c328af6fc8f46f950913f94f556
rosendo <rosendo@fullmoon.u-unistra.fr>
2018-04-18 12:18:02 +0200

    add_sonde v1

--- scripts/add_sonde.sh
+++ scripts/add_sonde.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-#yum -y update
-#yum -y install epel-release
-#yum -y install http://software.internet2.edu/rpms/el7/x86_64/main/RPMS/perfSONAR-repo-0.8-1.noarch.rpm
-#yum clean all
-#yum -y install perfsonar-toolkit
-
-#sleep 2
-
-#systemctl start pscheduler-scheduler
-#systemctl start pscheduler-runner
-#systemctl start pscheduler-archiver
-#systemctl start pscheduler-ticker
-#systemctl start perfsonar-lsregistrationdaemon
-#systemctl start bwctl-server
-#systemctl start owamp-server
-
+echo -e "Script pour l'ajout de nouvelles sondes perfSONAR à l'Observatoire ...\n"
 x=0
+f=0
 
-while getopts s:f option; do
-  case $option in
+while getopts "s:f:" option; do
+  case "${option}" in
     s)
       x=1
       SERVER=${OPTARG}
@@ -31,14 +17,46 @@ while getopts s:f option; do
   esac
 done
 
+function paquets {
+yum -y update
+yum -y install epel-release
+yum -y install http://software.internet2.edu/rpms/el7/x86_64/main/RPMS/perfSONAR-repo-0.8-1.noarch.rpm
+yum clean all
+yum -y install perfsonar-toolkit
+sleep 2
+systemctl start pscheduler-scheduler
+systemctl start pscheduler-runner
+systemctl start pscheduler-archiver
+systemctl start pscheduler-ticker
+systemctl start perfsonar-lsregistrationdaemon
+systemctl start bwctl-server
+systemctl start owamp-server
+}
+
 ip="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 
-if [ $x -eq 1 ] ; then
+reponse=2
+while [ $reponse -ne 1 ] && [ $reponse -ne 0 ] ; do
+  echo -e "Ce système a-t-il installé à partir de l'image CentOS Full Install ISO ? [1=oui/0=non]"
+  read reponse
+done
+
+if [ $reponse -eq 0 ] ; then
+  paquets
+fi
+
+if [ $x -eq 1 ] && [ $f -eq 1 ] ; then
+
 if [[ $SERVER =~ $ip ]] ; then
   if ping -c 1 $SERVER &> /dev/null ; then 
-#    ./maj_mesh-config.sh
     if curl -f http://$SERVER/$FICHIER ; then
-      echo "Le fichier n'existe pas."
+      echo -e "\n*******************************************************************\nFichier JSON trouvé ..."
+      sleep 2
+      echo -e "\nAppel au script de modification du fichier mesh config : $FICHIER ..."
+      sleep 2
+      ./maj_mesh-config.sh
+    else
+      echo -e "\nLe fichier entré n'existe pas dans le serveur !"
     fi
   else
     echo "Le serveur n'est pas disponible. Veulliez vérifier l'addresse ou essayez plus tard."
@@ -48,6 +66,6 @@ else
   exit 2
 fi
 else
-  echo -e "Vous devez entrer l'addresse du serveur MESH.\nUsage : $0 -s <address>"
+  echo -e "Vous devez entrer l'addresse du serveur MESH et le nom du fichier JSON.\nUsage : $0 -s <address> -f <fichier.JSON>"
 fi
 
--- scripts/maj_mesh-config.sh
+++ scripts/maj_mesh-config.sh
@@ -3,4 +3,4 @@
 #Script pour mettre à jour le fichier JSON ou en créer un nouveau
 #Fichier avec de tests par défaut
 
-
+echo -e "\nExecution du script de mise à jour ..."
