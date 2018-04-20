e02e4fb9c053ef2b3c84ab0e4ba629fa4c45b6c8
rosendo <rosendo@fullmoon.u-unistra.fr>
2018-04-20 16:17:19 +0200

    templating sonde

--- scripts/add_sonde.sh
+++ scripts/add_sonde.sh
@@ -4,6 +4,8 @@ echo -e "Script pour l'ajout de nouvelles sondes perfSONAR à l'Observatoire ...
 x=0
 f=0
 
+usage() { echo "Usage : $0 -s <address> -f <fichier.JSON>" 1>&2; exit 1;}
+
 while getopts "s:f:" option; do
   case "${option}" in
     s)
@@ -13,6 +15,9 @@ while getopts "s:f:" option; do
     f)
       f=1
       FICHIER=${OPTARG}
+      ;;
+    *)
+      usage
       ;;
   esac
 done
@@ -35,6 +40,11 @@ systemctl start owamp-server
 
 ip="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 
+
+if [ $x -eq 1 ] && [ $f -eq 1 ] ; then
+
+if [[ $SERVER =~ $ip ]] ; then
+
 reponse=2
 while [ $reponse -ne 1 ] && [ $reponse -ne 0 ] ; do
   echo -e "Ce système a-t-il installé à partir de l'image CentOS Full Install ISO ? [1=oui/0=non]"
@@ -45,16 +55,36 @@ if [ $reponse -eq 0 ] ; then
   paquets
 fi
 
-if [ $x -eq 1 ] && [ $f -eq 1 ] ; then
-
-if [[ $SERVER =~ $ip ]] ; then
   if ping -c 1 $SERVER &> /dev/null ; then 
     if curl -f http://$SERVER/$FICHIER ; then
       echo -e "\n*******************************************************************\nFichier JSON trouvé ..."
       sleep 2
+      echo -e "\n\nINFORMATION: UBICATION DE LA SONDE. Entrez la ville :"
+      read city
+      echo "INFORMATION: UBICATION DE LA SONDE. Entrez l'état :"
+      read state
+      echo "INFORMATION: UBICATION DE LA SONDE. Entrez la latitude :"
+      read lati
+      echo "INFORMATION: UBICATION DE LA SONDE. Entrez la longitude :"
+      read longi
+      echo "INFORMATION: Entrez une description :"
+      read descr
+      echo "INFORMATION: Entrez l'addresse de la sonde :"
+      read addr
+
+      echo "ville: "$city"" >> ./data.yaml
+      echo "etat: "$state"" >> ./data.yaml
+      echo "lat: "$lati"" >> ./data.yaml
+      echo "lon: "$longi"" >> ./data.yaml
+      echo "dec: "$descr"" >> ./data.yaml
+      echo "add: "$addr"" >> ./data.yaml
+
       echo -e "\nAppel au script de modification du fichier mesh config : $FICHIER ..."
       sleep 2
-      ./maj_mesh-config.sh
+      ./maj_mesh-config.py
+      echo -e "\n***********************************************************************\nNettoyage ..."
+      rm -f ./data.yaml
+      sleep 1
     else
       echo -e "\nLe fichier entré n'existe pas dans le serveur !"
     fi
--- scripts/maj_mesh-config.py
+++ scripts/maj_mesh-config.py
@@ -1,8 +1,17 @@
-print "Execution du script de mise à jour ..."
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print "\n*******************************************************************\nExecution du script de mise à jour ..."
+print "\nCréation de la configuration de la nouvelle sonde : \n"
 
-fichier=open("template.conf","r")
+import os
+from jinja2 import Environment, FileSystemLoader
+import yaml
+import commands
 
-for line in fichier.readlines():
-    print line
+config_data = yaml.load(open('./data.yaml'))
 
-fichier.close()
+env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+template = env.get_template('template.jinja2')
+
+print(template.render(config_data))
+
