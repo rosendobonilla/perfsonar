e5a179125085481e9c27be3573fd9296c74945e1
rosendo <rosendo@fullmoon.u-unistra.fr>
2018-04-23 14:19:28 +0200

    nouvelle add_sonde

---
+++ scripts/add_sonde-2.0.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+echo -e "Script pour l'ajout de nouvelles sondes perfSONAR à l'Observatoire ...\n"
+optSRV="0" ; optFICH="0"
+
+function aide { 
+   echo ""
+   echo "Usage : $0 -s <address> -f <fichier.JSON>" 1>&2; 
+   echo ""
+   echo "-s : L'addresse du serveur perfSONAR"
+   echo "-f : Le nom du fichier JSON"
+   echo ""
+}   
+
+if [ "$UID" -ne "0" ] ; then
+   echo -e "Vous devez être superutilisateur pour exécuter $0. \nEssayez avec sudo $0";
+   exit 9
+fi
+
+while getopts "s:f:" opts; do
+  case $opts in
+    s)
+      optSRV="1"
+      SERVER=${OPTARG}
+      ;;
+    f)
+      optFICH="1"
+      FICHIER=${OPTARG}
+      ;;
+    \?)
+      aide
+      exit 1
+      ;;
+  esac
+done
+
+
+if [ $optSRV == "1" ] && [ $optFICH == "1" ]; then
+
+if ping -c 1 $SERVER &> /dev/null ; then 
+  if curl -f http://$SERVER/$FICHIER ; then
+    echo -e "\n*******************************************************************\nFichier JSON trouvé ..."
+    sleep 2
+    
+    descr=$(whiptail --inputbox "Entrez une description pour la sonde." 8 78 --title "Information" 3>&1 1>&2 2>&3)
+                        
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+      echo "Description établie."
+    else
+      echo "Installation interrompue."
+      exit 1
+    fi
+    
+    addr=$(whiptail --inputbox "Entrez l'addresse de la sonde." 8 78 --title "Information" 3>&1 1>&2 2>&3)
+                                                            
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+      echo "Addresse établie."
+    else
+      echo "Installation interrompue."
+      exit 1
+    fi
+    
+    id=$(whiptail --inputbox "Entrez une identifient pour la sonde." 8 78 --title "Information" 3>&1 1>&2 2>&3)
+                        
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+      echo "Identifient établie."
+    else
+      echo "Installation interrompue."
+      exit 1
+    fi
+    
+    echo "id: "$id"" >> ./data.yaml
+    echo "desc: "$descr"" >> ./data.yaml
+    echo "add: "$addr"" >> ./data.yaml
+
+    echo -e "\nAppel au script de modification du fichier mesh config : $FICHIER ..."
+    sleep 2
+    ./maj_mesh-config.py "${$FICHIER}"
+    echo -e "\n***********************************************************************\nNettoyage ..."
+    rm -f ./data.yaml
+    rm -f mesh_tmp.conf
+    sleep 1
+  else
+    echo -e "\nLe fichier entré n'existe pas dans le serveur !"
+  fi
+else
+  echo "Le serveur n'est pas disponible. Veulliez vérifier l'addresse ou essayez plus tard."
+fi
+
+else
+   aide
+fi
+
--- scripts/maj_mesh-config.py
+++ scripts/maj_mesh-config.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 print "\n*******************************************************************\nExecution du script de mise à jour ..."
 print "\nCréation de la configuration de la nouvelle sonde : \n"
 
 import os
 from jinja2 import Environment, FileSystemLoader
 import yaml
-import commands
+import sys
+
+fich = sys.argv[1]
 
 config_data = yaml.load(open('./data.yaml'))
 
@@ -16,4 +19,19 @@ template = env.get_template('template.jinja2')
 print(template.render(config_data))
 
 print "\n******************************************************************\nConfiguration complète"
+print "\n******************************************************************\nModification du fichier mesh-central.json"
+
+file = open("mesh_tmp.conf","wb") 
+
+for line in open("fich").readlines():
+    file.write(line)
+    if line.startswith("#add_sonde"):
+        for line2 in open("template.jinja2").readlines():
+            file.write(line2)
+        file.write("#add_sonde\n")
+
+file.close
+
+os.system('sed -i "0,/#add_sonde/ s/#add_sonde//" fich')
+
 
