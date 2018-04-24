24fb302f76d90cf8c51bff7a4f1755c1f5d743f6
rosendo <rosendo@fullmoon.u-unistra.fr>
2018-04-24 16:37:36 +0200

    creation menu groupes

---
+++ scripts/menu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+choice=$(whiptail --title "Manage groups" --menu "Choisissez le groupe pour la nouvelle sonde" 16 78 5 \
+        "Interne" "Groupe sondes internes ObAS"\
+        "Exterieur" "Groupe sondes à l'exterieur" 3>&2 2>&1 1>&3) 
+         
+option=$(echo $choice | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
+
+echo $option
