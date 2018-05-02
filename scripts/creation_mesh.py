7776e36b8d66d7ca9f58058a9d3789860b126545
rosendo <rosendo@fullmoon.u-unistra.fr>
2018-05-02 09:41:00 +0200

    add comm

--- scripts/creation_mesh.py
+++ scripts/creation_mesh.py
@@ -14,13 +14,12 @@ import sys
 import glob
 
 #On récupère le chemin du répertoire MESH grace au script BASH
-
 reper = sys.argv[1]
 nomFich = reper + "/meshconfig.conf"
 
-##print "\n+-----------------------------------------------------------------+\nExecution du script de mise à jour ..."
 print "\nCréation de la configuration de la nouvelle sonde : \n"
 
+#Variable contenant l'entete du fichier meshconfig
 entete = """description PerfSONAR Observatoire Mesh Config
 
 <organization>
@@ -28,7 +27,7 @@ entete = """description PerfSONAR Observatoire Mesh Config
 
 """
 
-file = open(nomFich,"wb")                                                            #On crée le nouveau fichier de configuration
+file = open(nomFich,"wb")                                                            #On crée le nouveau fichier de configuration dans le chemin spécifié
 
 #L'entete va etre toujours la meme, on le met donc en brute dans le fichier
 file.write(entete)
@@ -44,10 +43,12 @@ for fich in sites:
         file.write(line)
 
 
+#On ferme le bloque
 file.write("</organization>\n\n")        
 
-file.write("<group obas_interne_mesh>\n")
-file.write("   type mesh\n")
+#On commence la partie des groupes
+file.write("<group obas_interne_mesh>\n")                                            #On utilise la meme methode pour obtenir les informations : dans ce cas, on parcours l'arborescence en cherchant les 
+file.write("   type mesh\n")                                                         #les membres du groupe mesh et les écrit dans le fichier
 for mem in glob.glob(reper + "/groupes/mesh/*"):
     dirname, filename = os.path.split(mem) 
     file.write("\n   member " + filename)
@@ -56,15 +57,15 @@ file.write("\n</group>")
 file.write("\n\n<group obas_exterieur_disjoint>\n")
 file.write("   type disjoint\n\n")
 
-for tipo in types:
-    for mem in glob.glob(reper + "/groupes/disjoint/" + tipo + "/*"):
+for tipo in types:                                                                   #On utilise la meme methode pour obtenir les informations : dans ce cas, on parcours l'arborescence en cherchant les
+    for mem in glob.glob(reper + "/groupes/disjoint/" + tipo + "/*"):                #les membres du groupe disjoint et les écrit dans le fichier
         dirname, filename = os.path.split(mem) 
         file.write("   " + tipo + " " + filename + "\n")
     file.write("\n")
 
 file.write("</group>\n\n")
 
-for line in open("../conf/body.cfg").readlines():
+for line in open("../conf/body.cfg").readlines():                                    #À la fin, on récupère les parties 'constantes' (les tests, etc)
     file.write(line)
 
 print "\n+-----------------------------------------------------------------+\nConfiguration complète"
