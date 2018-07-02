b3c713a10a86e83d0769b627219c6d7c562e4d2b
Rosendo Bonilla Juarez <bonilla.juarez.rosendo@gmail.com>
2018-07-02 11:16:17 +0200

    Update manage_sondes.sh

--- scripts/manage_sondes.sh
+++ scripts/manage_sondes.sh
@@ -506,29 +506,29 @@ elif [ $ACTION == "add" ] ; then
        die "Erreur produite peut-être à cause d'un probleme de droits sur le répertoire courant." 1
     fi
     tache_add
-    # if ! creation_json ; then
-    #    if ! recuperation ; then
-    #       die "Une erreur s'est produite pendant la récuperation de la configuration précedente. Vous avez le fichier $DIR.bak comme backup. Là dedans, vous avez toute votre configuration MESH precédente à la MàJ esssayée." 1
-    #    fi
-    #    die "Erreur dans la création de la nouvelle configuration pour le fichier JSON." 1
-    # fi
+    if ! creation_json ; then
+       if ! recuperation ; then
+          die "Une erreur s'est produite pendant la récuperation de la configuration précedente. Vous avez le fichier $DIR.bak comme backup. Là dedans, vous avez toute votre configuration MESH precédente à la MàJ esssayée." 1
+       fi
+       die "Erreur dans la création de la nouvelle configuration pour le fichier JSON." 1
+    fi
     apercu
-    # if ! redemarrer_serv_perfsonar ; then
-    #    die "Un erreur s'est produite pendant le rédemarrage des services perfSONAR." 1
-    # fi
+    if ! redemarrer_serv_perfsonar ; then
+       die "Un erreur s'est produite pendant le rédemarrage des services perfSONAR." 1
+    fi
 elif [ $ACTION == "delete" ] ; then
     echo "TACHE SUPRIMER UN SONDE"
     if ! tache_sup_sonde ; then die "Vous devez choisir la sonde à supprimer" 1 ; fi
     apercu
-    # if ! creation_json ; then
-    #    if ! recuperation ; then
-    #       die "Une erreur s'est produite pendant la récuperation de la configuration précedente. Vous avez le fichier $DIR.bak comme backup. Là dedans, vous avez toute votre configuration MESH precédente à la MàJ esssayée." 1
-    #    fi
-    #    die "Erreur dans la création de la nouvelle configuration pour le fichier JSON." 1
-    # fi
-    # if ! redemarrer_serv_perfsonar ; then
-    #    die "Un erreur s'est produite pendant le rédemarrage des services perfSONAR." 1
-    # fi
+    if ! creation_json ; then
+       if ! recuperation ; then
+          die "Une erreur s'est produite pendant la récuperation de la configuration précedente. Vous avez le fichier $DIR.bak comme backup. Là dedans, vous avez toute votre configuration MESH precédente à la MàJ esssayée." 1
+       fi
+       die "Erreur dans la création de la nouvelle configuration pour le fichier JSON." 1
+    fi
+    if ! redemarrer_serv_perfsonar ; then
+       die "Un erreur s'est produite pendant le rédemarrage des services perfSONAR." 1
+    fi
 
 elif [ $ACTION == "conftest" ] ; then
   if (whiptail --title "Manage tests" --yesno --no-button "Avancé" --yes-button "Suivant" "Normalement, cette partie est déjà configurée. Appuyez sur SUIVANT si vous voulez activer ou desactiver des tests ou sur AVANCÉ pour modifier les paramètres des tests." 10 78) then
@@ -541,15 +541,15 @@ elif [ $ACTION == "conftest" ] ; then
     ./creation_meshconfig.py "${DIR}" "${tests_mesh}" "${tests_disj}"
     apercu
   fi
-  # if ! creation_json ; then
-  #    if ! recuperation ; then
-  #       die "Une erreur s'est produite pendant la récuperation de la configuration précedente. Vous avez le fichier $DIR.bak comme backup. Là dedans, vous avez toute votre configuration MESH precédente à la MàJ esssayée." 1
-  #    fi
-  #    die "Erreur dans la création de la nouvelle configuration pour le fichier JSON." 1
-  # fi
-  # if ! redemarrer_serv_perfsonar ; then
-  #    die "Un erreur s'est produite pendant le rédemarrage des services perfSONAR." 1
-  # fi
+  if ! creation_json ; then
+     if ! recuperation ; then
+        die "Une erreur s'est produite pendant la récuperation de la configuration précedente. Vous avez le fichier $DIR.bak comme backup. Là dedans, vous avez toute votre configuration MESH precédente à la MàJ esssayée." 1
+     fi
+     die "Erreur dans la création de la nouvelle configuration pour le fichier JSON." 1
+  fi
+  if ! redemarrer_serv_perfsonar ; then
+     die "Un erreur s'est produite pendant le rédemarrage des services perfSONAR." 1
+  fi
 elif [ $ACTION == "meshfile" ] ; then
     apercu
 else
